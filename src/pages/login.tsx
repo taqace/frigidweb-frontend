b7243f9d3d6b8417f9715c98bfbb586568184d56
taq <taqace@gmail.com>
2021-07-27 20:51:21 -0400

    change/forgot password functionality

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form, Formik } from "formik";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useLoginMutation, useRegisterMutation } from "../generated/graphql";
@@ -8,6 +8,7 @@ import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/dist/client/router";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import NextLink from "next/link";
 
 interface loginProps {}
 
@@ -44,6 +45,11 @@ const Login: React.FC<{}> = ({}) => {
                                 type="password"
                             />
                         </Box>
+                        <Flex>
+                            <NextLink href="/forgot-password">
+                                <Link ml="auto">Forgot Password</Link>
+                            </NextLink>
+                        </Flex>
                         <Button
                             mt={4}
                             type="submit"
